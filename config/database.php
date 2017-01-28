c94607a987164971e5dc0a41865f916ed55036e9
Alver Alexander Grisales Ortega <viga.23@hotmail.com>
2017-01-28 00:56:19 -0500

    fixed bug user configuration mysql_tests

--- config/database.php
+++ config/database.php
@@ -70,7 +70,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE_TESTS', 'forge'),
+            'database' => env('DB_DATABASE_TESTS', 'sportsmate_test'),
             'username' => env('MYSQL_USER', env('DB_USERNAME', 'forge')),
             'password' => env('MYSQL_PASSWORD', env('DB_PASSWORD', '')),
             'charset' => 'utf8',
